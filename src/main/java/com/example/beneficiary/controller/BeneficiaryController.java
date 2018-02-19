384bdb53ea3cffe69ab607929ce3b114a9f8ff24
Fernando Ocampo <fernando.ocampo@ravencall.io>
2018-02-18 20:13:06 -0500

    Added skeleton for create beneficiary in the controller

--- src/main/java/com/example/beneficiary/controller/BeneficiaryController.java
+++ src/main/java/com/example/beneficiary/controller/BeneficiaryController.java
@@ -5,12 +5,28 @@
  */
 package com.example.beneficiary.controller;
 
+import com.example.beneficiary.model.Beneficiary;
+import com.example.beneficiary.model.Result;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * It is a REST interface to expose Beneficiary functionalities to the external 
  * world.
  * 
  * @author fernando.ocampo
  */
+@RestController
+@RequestMapping("/beneficiaries")
 public class BeneficiaryController {
     
+    @RequestMapping(value = "", method = RequestMethod.POST, 
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Result> create(@RequestBody Beneficiary beneficiary) {
+        return null;
+    }
 }
---
+++ src/main/java/com/example/beneficiary/model/Result.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.beneficiary.model;
+
+/**
+ *
+ * @author luisfer
+ */
+public class Result {
+    private String code;
+    private String message;
+
+    public Result() {
+    }
+
+    public Result(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
